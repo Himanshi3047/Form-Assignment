0e0af88e17047202e419ac79da4ad7532708c6e3
Himanshi3047 <132687489+Himanshi3047@users.noreply.github.com>
2023-08-03 20:50:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var form = document.getElementById('my-form');
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    alert("form submitted");
+    document.querySelector("button").setAttribute("href" , "");
+    //window.open ('https://ex.com' ,'_blank');
+   // window.open(form.action, 'Form submitted sucessfully');
+});
+
